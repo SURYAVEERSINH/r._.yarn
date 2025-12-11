6e13e085d89390699321e4681e7c038b57c50d10
SURYAVEERSINH <suryaveersinhbhati@gmail.com>
2025-12-12 01:24:09 +0530

    Update MongoDB connection string in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://suryaveersinhbhati:HARIDEV496@cluster0.xlrpaae.mongodb.net/r_yarn_shop');
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://suryaveersinhbhati:HARIDEV496@cluster0.x1rpaae.mongodb.net/r_yarn_shop');
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
